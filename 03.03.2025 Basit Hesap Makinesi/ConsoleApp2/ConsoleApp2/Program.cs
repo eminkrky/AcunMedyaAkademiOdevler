b96591368b5d8437eb55b587d9d13d92f8f42f23
eminkrky <afcan4455@gmail.com>
2025-03-19 14:04:17 +0300

    .

---
+++ 03.03.2025 Basit Hesap Makinesi/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Birinci tam sayıyı girin: ");
+        int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("İkinci tam sayıyı girin: ");
+        int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Yapmak istediğiniz işlemi seçin (+, -, *, /): ");
+        char islem = Convert.ToChar(Console.ReadLine());
+
+        if (islem == '+')
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine($"Toplama işlemi sonucu: {sonuc}");
+        }
+        else if (islem == '-')
+        {
+            int sonuc = sayi1 - sayi2;
+            Console.WriteLine($"Çıkarma işlemi sonucu: {sonuc}");
+        }
+        else if (islem == '*')
+        {
+            int sonuc = sayi1 * sayi2;
+            Console.WriteLine($"Çarpma işlemi sonucu: {sonuc}");
+        }
+        else if (islem == '/')
+        {
+            if (sayi2 != 0)
+            {
+                double sonuc = (double)sayi1 / sayi2;
+                Console.WriteLine($"Bölme işlemi sonucu: {sonuc}");
+            }
+            else
+            {
+                Console.WriteLine("Hata: Sıfıra bölme hatası!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Geçersiz işlem seçimi!");
+        }
+    }
+}
