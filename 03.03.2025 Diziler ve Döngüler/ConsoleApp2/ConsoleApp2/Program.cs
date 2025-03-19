f3af93e2947bbd41fe64ee637f2ce2ec68a44504
eminkrky <afcan4455@gmail.com>
2025-03-19 14:16:47 +0300

    .

---
+++ 03.03.2025 Diziler ve Döngüler/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int[] sayilar = new int[5];
+        int toplam = 0;
+        
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"Lütfen {i + 1}. sayıyı girin: ");
+            sayilar[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        
+        foreach (int sayi in sayilar)
+        {
+            toplam += sayi;
+        }
+        
+        double ortalama = (double)toplam / 5;
+        
+        Console.WriteLine($"Girilen sayıların toplamı: {toplam}");
+        Console.WriteLine($"Girilen sayıların ortalaması: {ortalama}");
+    }
+}
