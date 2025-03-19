eb4effdf7c8b3ae8c4822c82df1f78c93ec333e7
eminkrky <afcan4455@gmail.com>
2025-03-19 14:11:57 +0300

    .

---
+++ 03.03.2025 Sayı Tahmin Oyunu/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int hedefSayi = random.Next(1, 101);
+        int tahmin = 0;
+        int tahminSayisi = 0;
+
+        Console.WriteLine("1 ile 100 arasında bir sayı tahmin edin.");
+
+        while (tahmin != hedefSayi)
+        {
+            Console.Write("Tahmininiz: ");
+            tahmin = int.Parse(Console.ReadLine());
+            tahminSayisi++;
+
+            if (tahmin < hedefSayi)
+            {
+                Console.WriteLine("Daha büyük bir sayı girin.");
+            }
+            else if (tahmin > hedefSayi)
+            {
+                Console.WriteLine("Daha küçük bir sayı girin.");
+            }
+            else
+            {
+                Console.WriteLine($"Tebrikler! {tahminSayisi} tahminde doğru sonuca ulaştınız.");
+            }
+        }
+    }
+}
